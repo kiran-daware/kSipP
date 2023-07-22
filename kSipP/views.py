455f920522773e4bafa64dab12120ea2a0c90ca1
KiranDaware <KiranDaware@outlook.com>
2023-07-22 22:28:31 +0530

    new file:   kSipP/templates/modify_xml.html 	modified:   kSipP/views.py 	modified:   Logs/modifyHeader.log 	modified:   config.ini 	modified:   kSipP/__pycache__/forms.cpython-311.pyc 	modified:   kSipP/__pycache__/views.cpython-311.pyc 	modified:   kSipP/forms.py 	modified:   kSipP/scripts/modifyHeader.py 	modified:   kSipP/templates/run_script_template.html 	kSipP/scripts/xml/UAC_new.xml 	kSipP/sipp/ 	kSipP/templates/index.html

--- kSipP/views.py
+++ kSipP/views.py
@@ -2,35 +2,45 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.conf import settings
 from .forms import ConfigForm
+import configparser
 import os
 import subprocess
 
+config_file = os.path.join(str(settings.BASE_DIR), 'config.ini')
+config = configparser.ConfigParser()
+config.read(config_file)
+configd = config['DEFAULT']
+
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'index.html')
 
 
-def run_script_view(request):
+
+def modifyXml(request):
     if request.method == 'POST':
-        script_path = str(settings.BASE_DIR / 'kSipP' / 'scripts' / 'myScript.py')
+        whichXml = request.POST.get('xmlName')
+        modifyXml_script = str(settings.BASE_DIR / 'kSipP' / 'scripts' / 'modifyHeader.py')
         try:
-            result = subprocess.check_output(['python', script_path]).decode('utf-8').strip()
+            result = subprocess.run(['python', modifyXml_script], capture_output=True, text=True)
             return HttpResponse(result)
         except subprocess.CalledProcessError as e:
             # Handle any errors that may occur when running the script
             return HttpResponse(f"Error occurred: {e}")
-    
-    return render(request, 'run_script_template.html')
+        
+    return render(request, 'modify_xml.html')
 
 
-def modifyXml(request):
-    modifyXml_script = str(settings.BASE_DIR / 'kSipP' / 'scripts' / 'modifyHeader.py')
-    try:
-        result = subprocess.run(['python', modifyXml_script], capture_output=True, text=True)
-        return HttpResponse(result)
-    except subprocess.CalledProcessError as e:
-        # Handle any errors that may occur when running the script
-        return HttpResponse(f"Error occurred: {e}")
 
+config_data = {
+    'remoteAddr': configd.get('remoteAddr'),
+    'remotePort': configd.get('remotePort'),
+    'srcAddrUac': configd.get('srcAddrUac'),
+    'srcPortUac': configd.get('srcPortUac'),
+    'srcAddrUas': configd.get('srcAddrUas'),
+    'srcPortUas': configd.get('srcPortUas'),
+}
 
 
 def write_config(request):
@@ -40,15 +50,18 @@ def write_config(request):
             # Get the configuration data from the form
             remoteAddr = form.cleaned_data['remoteAddr']
             remotePort = form.cleaned_data['remotePort']
-            srcAddr = form.cleaned_data['srcAddr']
-            srcPort = form.cleaned_data['srcPort']
+            srcAddrUac = form.cleaned_data['srcAddrUac']
+            srcPortUac = form.cleaned_data['srcPortUac']
+            srcAddrUas = form.cleaned_data['srcAddrUas']
+            srcPortUas = form.cleaned_data['srcPortUas']
 
             # Get more settings from the form as needed
 
             # Create the configuration content
             config_content = f'''[DEFAULT]\n
 remoteAddr = {remoteAddr}\nremotePort = {remotePort}
-srcAddr = {srcAddr}\nsrcPort = {srcPort}'''
+srcAddrUac = {srcAddrUac}\nsrcPortUac = {srcPortUac}
+srcAddrUas = {srcAddrUas}\nsrcPortUAS = {srcPortUas}'''
 
             # Add more settings to the config_content as needed
 
@@ -61,8 +74,33 @@ srcAddr = {srcAddr}\nsrcPort = {srcPort}'''
 
             return render(request, 'success_template.html')
     else:
-        form = ConfigForm()
+        form = ConfigForm(initial=config_data)
 
     return render(request, 'config_template.html', {'form': form})
 
 
+
+def run_script_view(request):
+    if request.method == 'POST':
+        scriptName = request.POST.get('script')
+
+        sipp = str(settings.BASE_DIR / 'kSipP' / 'sipp' / 'sipp.exe')
+        uacXml = str(settings.BASE_DIR / 'kSipP' / 'sipp' / 'UAC.xml')
+        uasXml = str(settings.BASE_DIR / 'kSipP' / 'sipp' / 'UAS.xml')
+        remote=f"{config_data['remoteAddr']}:{config_data['remotePort']}"
+        uacSrc=f"-i {config_data['srcAddrUac']} -p {config_data['srcPortUac']}"
+        uasSrc=f"-i {config_data['srcAddrUas']} -p {config_data['srcPortUas']}"
+        
+        if scriptName == 'UAC':
+            try:
+                subprocess.Popen(["start", "cmd.exe", "/K", f"{sipp} -sf {uacXml} {remote} {uacSrc} -m 1"], shell=True)
+            except Exception as e:
+                print(f"Error: {e}")
+            
+        if scriptName =='UAS':
+            try:
+                subprocess.Popen(["start", "cmd.exe", "/K", f"{sipp} -sf {uasXml} {remote} {uasSrc}"], shell=True)
+            except Exception as e:
+                print(f"Error: {e}")
+    
+    return render(request, 'run_script_template.html')
