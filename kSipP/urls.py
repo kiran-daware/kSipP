ece05ac21191780d8de2a69e71e7e3773bb32595
kiran daware <kiran.daware@outlook.com>
2025-05-13 14:07:08 +0530

    Code Clean-up 	modified:   config.ini 	deleted:    kSipP/templates/modify_xml.html 	deleted:    kSipP/templates/run_script_template.html 	deleted:    kSipP/templates/show_xml_flow.html 	modified:   kSipP/urls.py 	modified:   kSipP/views.py

--- kSipP/urls.py
+++ kSipP/urls.py
@@ -3,9 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('modify-xml/', views.modifyXml, name='modify-xml'),
     path('xml/<str:xmlname>/', views.serveXmlFile, name='xml-file'),
-    # path('xml-editor/', views.aceXmlEditor, name='xml-editor'),
     path('edit-xml/', views.xmlEditor, name='edit-xml'),
     path('log/<int:pid>/<str:xml>/', views.display_sipp_screen, name='display_sipp_screen'),
     path('xml-management/', views.xml_management, name='xml-management'),
